5c79bfd71a8393d64d1bdada25e6a2517c5a38c8
Diego Granados <diegranados@estudiantec.cr>
2024-10-27 20:10:33 -0600

    start on pruebas file

--- main.c
+++ main.c
@@ -3,135 +3,119 @@
 #include <string.h>
 #include "file_system.c"
 
+#define MAX_FILES 100
+#define MAX_FILENAME 50
+#define MAX_CONTENT_SIZE 1000
+
 // Funcion para procesar cada linea
-// void processLine(const char *line)
-// {
-//   char command[10]; // Buffer para almacenar el comando
-//   char name;        // Variable para el nombre (carácter)
+void processLine(const char *instruction)
+{
+    if (instruction[0] == '\0' || strlen(instruction) == 0) {
+        return;
+    }
 
-//   size_t size; // Variable para el tamaño
+    if (instruction[0] == '#') {
+        return;
+    }
 
-//   // Parsea la linea (con ALLOC, REALLOC y FREE)
-//   if (sscanf(line, "%s %c %zu", command, &name, &size) == 3) // Usa &name para pasar la dirección de name
-//   {
-//     if (strcmp(command, "ALLOC") == 0)
-//     {
-//       allocateMemory(name, size);
-//     }
-//     else if (strcmp(command, "REALLOC") == 0)
-//     {
-//       reallocateMemory(name, size);
-//     }
-//   }
-//   else if (sscanf(line, "%s %c", command, &name) == 2)
-//   {
-//     if (strcmp(command, "FREE") == 0)
-//     {
-//       freeMemory(name);
-//     }
-//   }
-//   // Caso de PRINT que no requiere más argumentos
-//   else if (strcmp(command, "PRINT") == 0)
-//   {
-//     printf("Lista de memoria asignada:\n");
-//     printList(assignedList);
-//     printf("Lista de memoria no asignada o huecos:\n");
-//     printList(unassignedList);
-//     printMemory();
-//     // printMemory();
-//   }
-//   else if (strcmp(command, "RESET") == 0)
-//   {
-//     reset();
-//   }
-//   else
-//   {
-//     printf("Invalid command: %s\n", line); // Mensaje de error si el comando no es válido
-//   }
-// }
+    // Print lines that start with '+'
+    if (instruction[0] == '+') {
+        printf("%s\n", instruction + 1);  // Print without the '+'
+        return;
+    }
 
-// // Funcion para leer las lineas del archivo
-// void readLinesFromFile(const char *filename)
-// {
-//   FILE *file = fopen(filename, "r");
-//   if (file == NULL)
-//   {
-//     perror("Error opening the file");
-//     exit(EXIT_FAILURE);
-//   }
+    char command[10], name[MAX_FILENAME], data[MAX_CONTENT_SIZE];
+    int offset, size, length;
+    
+    if (sscanf(instruction, "CREATE %s %d", name, &size) == 2) {
+        createFile(name, size);
+    } else if (sscanf(instruction, "WRITE %s %d \"%[^\"]\"", name, &offset, data) == 3) {
+        writeFile(name, offset, data);
+    } else if (sscanf(instruction, "READ %s %d %d", name, &offset, &length) == 3) {
+        // readFile(name, offset, length);
+        return;
+    } else if (strncmp(instruction, "LIST", 4) == 0) {
+        listFiles();
+    } else if (strncmp(instruction, "RESET", 5) == 0) {
+        reset();
+    } else if (strncmp(instruction, "READ", 4) == 0) {
+        read();
+    } else if (sscanf(instruction, "DELETE %s", name) == 1) {
+        deleteFile(name);
+    } else {
+        printf("Error: Unknown instruction '%s'\n", instruction);
+    }
+}
 
-//   char line[50]; // Buffer para almacenar cada línea
+    // Funcion para leer las lineas del archivo
+void readLinesFromFile(const char *filename)
+    {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        perror("Error opening the file");
+        exit(EXIT_FAILURE);
+    }
 
-//   // Leer línea por línea del archivo
-//   while (fgets(line, sizeof(line), file))
-//   {
-//     if (line[0] == '#' || line[0] == '\n')
-//     {
-//       continue; // Ignorar comentarios
-//     }
-//     else if (line[0] == '+')
-//     {
-//       printf("\n%s", line);
-//       continue; // Ignorar líneas en blanco
-//     }
-//     processLine(line); // Procesar la línea
-//   }
+    char instruction[256];
+    while (fgets(instruction, sizeof(instruction), file)) {
+        instruction[strcspn(instruction, "\n")] = '\0';  // Remove newline character
+        processLine(instruction);
+    }
 
-//   fclose(file); // Cerrar el archivo
-// }
+    fclose(file); // Cerrar el archivo
+}
 
 int main(int argc, char *argv[])
 {
-  
-  disk = fopen("disk.txt", "r+b"); // Use "r+b" to read/write in binary mode
-  if (disk == NULL) {
-      perror("Error opening file");
-      return EXIT_FAILURE;
-  }
-  FATFile = fopen("FAT.txt", "r+b");
-  if (FATFile == NULL) {
-      perror("Error opening file");
-      return EXIT_FAILURE;
-  }
-  directoryFile = fopen("directory.txt", "r+");
-  if (directoryFile == NULL) {
-      perror("Error opening file");
-      return EXIT_FAILURE;
-  }
+    // if (argc < 2)
+    // {
+    //     printf("Uso: %s <nombre del archivo>\n", argv[0]);
+    //     return 1;
+    // }
 
-  directory = createFileList();
-  openFiles = createFileList();
-  freeBlocks = createBlockList();
-  resetDirectory();
-  resetFAT();
-  resetDisk();
-  readFAT();
-  readDirectory();
+    disk = fopen("disk.txt", "r+b"); // Use "r+b" to read/write in binary mode
+    if (disk == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+    FATFile = fopen("FAT.txt", "r+b");
+    if (FATFile == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+    directoryFile = fopen("directory.txt", "r+");
+    if (directoryFile == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
 
-  createFile("file1.txt", 1500);
-  createFile("file2.txt", 2000);
-  createFile("file3.txt", 1000);
-  deleteFile("file2.txt");
-  createFile("file4.txt", 2500);
-  listFiles();
-  // writeFile("file1.txt", 0, "Hola mundo");
-  fclose(disk);
-  fclose(FATFile);
-  fclose(directoryFile);
-  
-  // if (argc < 2)
-  // {
-  //   printf("Uso: %s <nombre del archivo>\n", argv[0]);
-  //   return 1;
-  // }
+    directory = createFileList();
+    openFiles = createFileList();
+    freeBlocks = createBlockList();
 
-  // // Crear las listas globales
-  // createList(&assignedList);
-  // createList(&unassignedList);
-  // startMemory();
-  // addValue(unassignedList, '0', 0, MEMORY_SIZE);
+    // readLinesFromFile(argv[1]);
+    readLinesFromFile("pruebas.txt");
+    // resetDirectory();
+    // resetFAT();
+    // resetDisk();
+    // readFAT();
+    // readDirectory();
 
-  // readLinesFromFile(argv[1]);
+    // createFile("file1.txt", 1500);
+    // createFile("file2.txt", 2000);
+    // createFile("file3.txt", 1000);
+    // deleteFile("file2.txt");
+    // createFile("file4.txt", 2500);
+    // listFiles();
+    // writeFile("file1.txt", 0, "Hola mundo");
+    fclose(disk);
+    fclose(FATFile);
+    fclose(directoryFile);
 
-  // return 0;
+
+
+
+
+    return 0;
 }
